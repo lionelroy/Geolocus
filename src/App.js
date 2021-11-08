5b6fcf75c812b594d77efc04a0206fe265a5ca87
Stephane Roy <stephroy411@hotmail.com>
2021-11-08 15:33:51 -0400

    Add Cards in the list component with data from the travel advisor API.

--- src/App.js
+++ src/App.js
@@ -8,15 +8,23 @@ import Map from './components/Map/Map';
 
 const App = () => {
   const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
 
   useEffect(() => {
-    getPlacesData()
+    navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
+      setCoordinates({ lat: latitude, lng: longitude });
+    })
+  }, [])
+
+  useEffect(() => {
+    getPlacesData(bounds.sw, bounds.ne)
       .then((data) => {
         console.log(data);
 
         setPlaces(data);
       })
-  }, []);
+  }, [coordinates, bounds]);
 
   return (
     <>
@@ -24,10 +32,14 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: '100%'}}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map 
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
         </Grid>
       </Grid> 
     </>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -5,26 +5,14 @@ import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
 import useStyles from './styles';
 
-const List = () => {
+const List = ({ places }) => {
   const classes = useStyles();
   const [type, setType] = useState('restaurants');
   const [rating, setRating] = useState('');
 
-  const places = [
-    { name: 'French cuisine'},
-    { name: 'Best Beer'},
-    { name: 'Best Pizza'},
-    { name: 'Jazz night'},
-    { name: 'Breakfast'},
-    { name: 'Vegan'},
-    { name: 'Pet friendly'},
-    { name: 'Garden Terrasse'},
-    { name: 'Korean BBQ'},
-  ];
-
   return (
     <div className={classes.container}>
-      <Typography variant="h4">Restaurants, Hotels & Attractions around you.</Typography> 
+      <Typography variant="h4">Restaurants, Hotels & Attractions around the world.</Typography> 
       <FormControl className={classes.formControl}>
         <InputLabel>Type</InputLabel>
         <Select value={type} onChange={(e) => setType(e.target.value)}>
